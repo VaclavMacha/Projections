b0741a95f81e1b5e941bd7caab2d5f516f938a5b
VaclavMacha <vaclav.macha92@gmail.com>
2019-04-09 08:41:24 +0800

    Removed trash and old tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,24 +1,5 @@
 using Projections, Test, Distributions, LinearAlgebra
 
-# include("test_projections.jl")
-#
-# function testerror(x, x0)
-# 	abs_error = abs(x - x0)
-# 	max_abs   = max(abs(x), abs(x0))
-# 	rel_error = iszero(max_abs) ? 0 : abs_error/max_abs
-# 	return min(abs_error, rel_error)
-# end
-#
-# @time @testset "Pat@Mat:" begin
-# 	@time test_PatMat_projection_hinge()
-# 	@time test_PatMat_projection_quadratic()
-# end;
-#
-# @time @testset "TopRank:" begin
-# 	@time test_TopRank_projection_hinge()
-# 	@time test_TopRank_projection_quadratic()
-# end;
-
 include("tests.jl")
 
 @testset "All projections tests" begin

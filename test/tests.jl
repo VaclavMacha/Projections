80ed8388fa34235942beea61053c9666372f0aaf
Vaclav Macha <vaclav.macha92@gmail.com>
2019-10-13 19:50:29 +0200

    Improved tests.

--- test/tests.jl
+++ test/tests.jl
@@ -18,7 +18,7 @@ end
 methods() = vcat(divergences(), norms())
 
 
-function test_generate(; atol = 1e-4)
+function test_generate(; atol::Real = 1e-4)
     Test.@testset "Test of generating functions" begin
         Test.@testset "$(Projections.name(d))" for d in divergences()
             ϕ = Projections.generate(d)
@@ -29,7 +29,7 @@ function test_generate(; atol = 1e-4)
 end
 
 
-function test_feasibility(d::Divergence, m::Model, p; atol = 1e-4)
+function test_feasibility(d::Divergence, m::Model, p; atol::Real = 1e-6)
     ϕ = Projections.generate(d)
     Test.@testset "Feasibility" begin
         Test.@test isapprox(sum(p), 1; atol = atol)
@@ -40,7 +40,7 @@ function test_feasibility(d::Divergence, m::Model, p; atol = 1e-4)
 end
 
 
-function test_feasibility(d::Norm, m::Model, p; atol = 1e-4)
+function test_feasibility(d::Norm, m::Model, p; atol::Real = 1e-6)
     k = Projections.normtype(d)
     Test.@testset "Feasibility" begin
         Test.@test isapprox(sum(p), 1; atol = atol)
@@ -51,7 +51,7 @@ function test_feasibility(d::Norm, m::Model, p; atol = 1e-4)
 end
 
 
-function isfeasible(d::Divergence, m::Model, p; atol = 1e-4)
+function isfeasible(d::Divergence, m::Model, p; atol::Real = 1e-6)
     ϕ = Projections.generate(d)
     return all([isapprox(sum(p), 1; atol = atol),
                 sum(m.q.*ϕ.(p./m.q)) <= 1.01 * m.ε,
@@ -59,7 +59,7 @@ function isfeasible(d::Divergence, m::Model, p; atol = 1e-4)
 end
 
 
-function isfeasible(d::Norm, m::Model, p; atol = 1e-4)
+function isfeasible(d::Norm, m::Model, p; atol::Real = 1e-6)
     k = Projections.normtype(d)
     return all([isapprox(sum(p), 1; atol = atol),
                 LinearAlgebra.norm(p - m.q, k) <= 1.01 * m.ε,
@@ -68,24 +68,24 @@ end
 
 
 function test_optimality(d::Union{Divergence, Norm}, m::Model, p, psolver; atol = 1e-4)
-    isfeasible(d, m, psolver; atol = 1e-4) || return 
+    isfeasible(d, m, psolver; atol = atol) || return 
     
     Test.@testset "Optimality" begin
-        Test.@test isapprox(m.c'*p, m.c'*psolver, atol = atol) || m.c'*p >= m.c'*psolver - atol
+        Test.@test m.c'*p >= m.c'*psolver - atol
     end
     return
 end
 
 
-function test_model(m::Model; atol = 1e-4)
+function test_model(m::Model)
     Test.@testset "Comparison with the general solver" begin
         Test.@testset "$(Projections.name(d))" for d in methods()
             
             p       = solve(d,m);
             psolver = solve_exact(d,m);
 
-            test_feasibility(d, m, p)
-            test_optimality(d, m, p, psolver)
+            test_feasibility(d, m, p; atol = 1e-6)
+            test_optimality(d, m, p, psolver; atol = 1e-4)
         end
     end
     return

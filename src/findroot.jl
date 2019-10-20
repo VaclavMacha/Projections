3796fce976cc31d40e9cfc6dae6fcc1fa70f60ee
Vaclav Macha <vaclav.macha92@gmail.com>
2019-10-20 18:59:37 +0200

    Improved stopping criterion for the newton method.

--- src/findroot.jl
+++ src/findroot.jl
@@ -1,38 +1,43 @@
 """
-    newton(d::Union{ChiSquare, ModifiedChiSquare}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-10)
+    newton(d::Union{ChiSquare, ModifiedChiSquare}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-8)
 
 A simple bisection method for finding the root of the function `h` for the DRO problem with
 χ²-distance or modified χ²-distance.
 """
-function newton(d::Union{ChiSquare, ModifiedChiSquare}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-10)
+function newton(d::Union{ChiSquare, ModifiedChiSquare}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-8)
     stats.optimizer  = "newton"
     f(λ)  = h(d, m, λ)
     ∇f(λ) = ∇h(d, m, λ)
 
-    λ     = initial(d,m)
+    λ     = initial(d,m)    
     f_val = f(λ)
     add_eval()
 
     f_val == 0 && return λ
 
     for k in 1:maxiter
-        abs(f_val) > atol || break
+        abs(f_val) > atol || return λ
 
-        λ    -= f_val/∇f(λ)
+        Δλ = f_val/∇f(λ)
+        abs(Δλ) > atol*1e-2 || return λ
+        
+        λ    -= Δλ
         f_val = f(λ)
         add_eval()
     end
+    @warn "Newton method for the DRO model with $(name(d)) reached maximal
+           number of iteration. The solution may not be accurate: h(μ) = $(f(λ))" 
     return λ
 end
 
 
 """
-    newton(d::Ltwo, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-10)
+    newton(d::Ltwo, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-8)
 
 A simple bisection method for finding the root of the function `h` for the DRO problem with
 l-2 norm.
 """
-function newton(d::Ltwo, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-10)
+function newton(d::Ltwo, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-8)
     stats.optimizer  = "newton"
     f(μ, λ)  = h(d, m, μ; λ = λ)
     ∇f(μ, λ) = ∇h(d, m, μ; λ = λ)
@@ -45,24 +50,29 @@ function newton(d::Ltwo, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-10)
     f_val == 0 && return μ
 
     for k in 1:maxiter
-        abs(f_val) > atol || break
+        abs(f_val) > atol || return μ
 
-        μ    -= f_val/∇f(μ, λ)
+        Δμ = f_val/∇f(μ, λ)
+        abs(Δμ) > atol*1e-2 || return λ
+        
+        μ    -= Δμ
         λ     = g(d, m, μ)
         f_val = f(μ, λ)
         add_eval()
     end
+    @warn "Newton method for the DRO model with $(name(d)) reached maximal
+           number of iteration. The solution may not be accurate: h(μ) = $(f(μ))"
     return μ
 end
 
 
 """
-    bisection(d::Union{KullbackLeibler, Burg, Hellinger}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-10)
+    bisection(d::Union{KullbackLeibler, Burg, Hellinger}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-8)
 
 A simple bisection method for finding the root of the function `h` for the DRO problem with
 Kullback-Leibler divergence, Burg entropy or Hellinger distance.
 """
-function bisection(d::Union{KullbackLeibler, Burg, Hellinger}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-10)
+function bisection(d::Union{KullbackLeibler, Burg, Hellinger}, m::DRO; maxiter::Integer = 1000, atol::Real = 1e-8)
     stats.optimizer  = "bisection"
     f(μ) = h(d, m, μ)
     a, b = bounds(d,m)
@@ -77,13 +87,13 @@ function bisection(d::Union{KullbackLeibler, Burg, Hellinger}, m::DRO; maxiter::
     f_b == 0 && return b
 
     for k in 1:maxiter
-        abs(b - a) > atol || break
+        abs(b - a) > atol || return c
 
         c   = Statistics.middle(a, b)
         f_c = f(c)
         add_eval()
 
-        f_c == 0 && break
+        f_c == 0 && return c
 
         if f_a*f_c > 0
             a, f_a = c, f_c
@@ -91,5 +101,7 @@ function bisection(d::Union{KullbackLeibler, Burg, Hellinger}, m::DRO; maxiter::
             b = c
         end
     end
+   @warn "Bisection method for the DRO model with $(name(d)) reached maximal
+           number of iteration. The solution may not be accurate: h(μ) = $(f(c))"
     return c
 end

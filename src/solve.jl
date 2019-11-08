e601c49f19d2f6258523973e2164032567a58020
Vaclav Macha <vaclav.macha92@gmail.com>
2019-11-08 09:36:26 +0100

    Add check for Philpott solver.

--- src/solve.jl
+++ src/solve.jl
@@ -3,8 +3,10 @@
 
 Solves given DRO model `m` using solver `s`. 
 """
-function solve(s::Solver, m::DRO; kwargs...)
+function solve(s::S, m::DRO; kwargs...) where {S<:Solver}
     reset_stats(s, m)
+
+    @assert S <: Philpott && typeof(m.d) <: Ltwo "Philpott solver is supported only for the DRO model with l2 norm"
 
     p, t, bytes, gctime, memallocs = @timed optimal(s, m.d, m; kwargs...)
     add_stats(t, bytes)
@@ -18,8 +20,10 @@ end
 
 Solves given Simplex model `m` using solver `s`. 
 """
-function solve(s::Solver, m::Simplex; kwargs...)
+function solve(s::S, m::Simplex; kwargs...) where {S<:Solver}
     reset_stats(s, m)
+
+    @assert !(S <: Philpott) "Philpott solver is supported only for the DRO model with l2 norm"
 
     p, t, bytes, gctime, memallocs = @timed optimal(s, m; kwargs...)
     add_stats(t, bytes)
